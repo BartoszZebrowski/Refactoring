28915b6de9df9260db61306ad2f632fa1a8c7f07
s24708 <s24708@LAB359-12>
2024-03-24 18:15:24 +0100

    Add project files.

---
+++ LegacyApp/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace LegacyApp
+{
+    public class User
+    {
+        public object Client { get; internal set; }
+        public DateTime DateOfBirth { get; internal set; }
+        public string EmailAddress { get; internal set; }
+        public string FirstName { get; internal set; }
+        public string LastName { get; internal set; }
+        public bool HasCreditLimit { get; internal set; }
+        public int CreditLimit { get; internal set; }
+
+        public User(string firstName, string lastName, string emailAdress, DateTime dateOfBirth, object client) 
+        {
+            if (!Validate(firstName, lastName, emailAdress, dateOfBirth, client))
+                throw new Exception("Wrong user data");
+
+            FirstName = firstName;
+            LastName = lastName;
+            EmailAddress = emailAdress;
+            DateOfBirth = dateOfBirth;
+            Client = client;
+        }
+
+        private bool Validate(string firstName, string lastName, string emailAdress, DateTime dateOfBirth, object client)
+        {
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName))
+            {
+                return false;
+            }
+
+            if (!emailAdress.Contains("@") && !emailAdress.Contains("."))
+            {
+                return false;
+            }
+
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day)) age--;
+
+            if (age < 21)
+            {
+                return false;
+            }
+        }
+    }
+}
